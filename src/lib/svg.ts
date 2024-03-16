7df1eaf7fa9736655371a2dca34dd4f0cb161d2e
eyemono-moe <ysdr83@gmail.com>
2024-03-17 07:04:59 +0900

    Optimize SVG

--- src/lib/svg.ts
+++ src/lib/svg.ts
@@ -3,12 +3,21 @@ import { optimize } from "svgo";
 export const optimizeSvg = (svgText: string) =>
   optimize(svgText, {
     plugins: [
+      "removeEmptyContainers",
       "removeComments",
       "removeUselessDefs",
+      "collapseGroups",
+      "cleanupNumericValues",
       {
         name: "removeUnknownsAndDefaults",
         params: {
           keepDataAttrs: false,
+        },
+      },
+      {
+        name: "removeComments",
+        params: {
+          preservePatterns: false,
         },
       },
     ],
