09aa12a911a862c536e4a9b8a62cfa3d3d76257b
eyemono-moe <ysdr83@gmail.com>
2024-08-10 00:42:04 +0900

    optimize bundle option

--- app.config.ts
+++ app.config.ts
@@ -1,11 +1,22 @@
 import { defineConfig } from "@solidjs/start/config";
 import unoCss from "unocss/vite";
+import { visualizer } from "rollup-plugin-visualizer";
 
 export default defineConfig({
   server: {
     preset: "vercel",
   },
   vite: {
-    plugins: [unoCss()],
+    plugins: [unoCss(), visualizer()],
+    build: {
+      rollupOptions: {
+        output: {
+          manualChunks(id) {
+            if (id.includes("css-tree")) return "css-tree";
+            if (id.includes("svgo")) return "svgo";
+          },
+        },
+      },
+    },
   },
 });
